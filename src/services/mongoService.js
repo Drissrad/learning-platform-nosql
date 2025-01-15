80527f0be81a0758132733566b813fcddc7cfba8
drissrad <bonnedebut2@gmail.com>
2025-01-15 18:18:51 +0100

    Mise à jour des services MongoDB et Redis

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -2,13 +2,42 @@
 // Réponse: 
 
 const { ObjectId } = require('mongodb');
-
+const db = require('../config/db').getDb();
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
+  try {
+    if (!ObjectId.isValid(id)) {
+      throw new Error('ID non valide.');
+    }
+    const result = await db.collection(collection).findOne({ _id: new ObjectId(id) });
+    return result;
+  } catch (err) {
+    console.error(`Erreur lors de la recherche dans ${collection} :`, err);
+    throw err;
+  }
+}
+async function insertOne(collection, data) {
+  try {
+    const result = await db.collection(collection).insertOne(data);
+    return result;
+  } catch (err) {
+    console.error(`Erreur lors de l'insertion dans ${collection} :`, err);
+    throw err;
+  }
+}
+async function findAll(collection) {
+  try {
+    const results = await db.collection(collection).find().toArray();
+    return results;
+  } catch (err) {
+    console.error(`Erreur lors de la récupération dans ${collection} :`, err);
+    throw err;
+  }
 }
 
 // Export des services
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  findOneById,
+  insertOne,
+  findAll,
 };
--- src/services/redisService.js
+++ src/services/redisService.js
@@ -4,10 +4,24 @@
 // Réponse :
 
 // Fonctions utilitaires pour Redis
+const redisClient = require('../config/db').redisClient;
 async function cacheData(key, data, ttl) {
-    // TODO: Implémenter une fonction générique de cache
+  try {
+    await redisClient.setEx(key, ttl, JSON.stringify(data));
+  } catch (err) {
+    console.error('Erreur lors de la mise en cache :', err);
+  }
+  }
+  async function getCachedData(key) {
+    try {
+      const data = await redisClient.get(key);
+      return data ? JSON.parse(data) : null;
+    } catch (err) {
+      console.error('Erreur lors de la récupération du cache :', err);
+      return null;
+    }
   }
   
   module.exports = {
-    // TODO: Exporter les fonctions utilitaires
+    cacheData, getCachedData
   };
